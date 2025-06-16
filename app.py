93dca9bacf2f8fa72bca7e971c61c123b239ebe5
Sparsh Sharma <sparshsharma2407@gmail.com>
2025-06-16 14:41:59 +0530

    Fixed unresolved merge markers in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -28,25 +27,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    input_data = [float(x) for x in request.form.values()]
-    final_input = [np.array(input_data)]
-    prediction = model.predict(final_input)[0]
-    return render_template('index.html', prediction_text=f'Predicted Price: ${prediction:.2f}')
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> d54ee14d440e3f1d820118828453e620fc6d549a
